3f292a5bf6ffb37076e9f3f3d8f0e552cceb374b
Abhee Hudani <62197910+Novartus@users.noreply.github.com>
2022-06-02 02:31:33 -0400

    Update sum_md5.sh

--- ASP_2/sum_md5.sh
+++ ASP_2/sum_md5.sh
@@ -1,13 +1,20 @@
-#/bin/bash
-echo “Enter dir”
-read dir
- 
-cd $dir
- 
+#!/usr/bin/env bash
+cd $1
+filename='/mnt/c/Users/hudan/Desktop/ASP_2/md5List.txt'
+
+if [[ ! -e $filename ]]; then
+# Create file if it doesn't exist
+    touch $filename
+else 
+# Empty the file if it exists
+    : > $filename
+fi
+
 for i in $(find $dir -type f);
 do
     md5=`md5sum ${i} | awk '{ print $1 }'`
     owner=$(stat -c '%G' ${i})
     filePath=$(realpath ${i})
-    echo "$filePath:$owner:$md5"; 
+    printValue="$filePath:$owner:$md5"
+    echo "$printValue" >> $filename
 done;
