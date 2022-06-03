330e685e0ee74ed5dc7792f3efabb0d2e6cd971e
Abhee Hudani <62197910+Novartus@users.noreply.github.com>
2022-06-03 15:56:03 -0400

    Update sum_md5.sh

--- ASP_2/sum_md5.sh
+++ ASP_2/sum_md5.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 cd $1
-filename='/mnt/c/Users/hudan/Desktop/ASP_2/md5List.txt'
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )"; # Get script directory
+# filename='/mnt/c/Users/hudan/Desktop/ASP_2/md5List.txt'
+filename="$SCRIPT_DIR/md5List.txt"
 
 if [[ ! -e $filename ]]; then
 # Create file if it doesn't exist
