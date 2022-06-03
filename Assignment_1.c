e1b27dfaa6f1f9c166870b8a6bb194242efaa227
Abhee Hudani <62197910+Novartus@users.noreply.github.com>
2022-06-03 12:11:08 -0400

    Update Assignment_1.c

--- Assignment_1.c
+++ Assignment_1.c
@@ -1,5 +1,6 @@
 #define _GNU_SOURCE
 #define _XOPEN_SOURCE 500
+
 #include <ftw.h>
 
 #include <stdio.h>
@@ -12,49 +13,111 @@
 
 #include <unistd.h>
 
-char ** globalArgV;
+#include <stdbool.h>
+
+char **globalArgV;
 int target = -1;
-char * startPath = ".";
+char *startPath = ".";
 int count = 0;
 
-int customNftw(const char * fName,
-  const struct stat * statPtr, int fileFlags, struct FTW * pfwt) {
-  if (fileFlags == FTW_SL) { // if it is a symbolic link
-    char * fullpath = realpath(fName, NULL); // character pointer to the full path of the file from the symbolic link
-    char * dName = dirname(fullpath); // character pointer to the directory name to check if real path matches any entry in the ancestry.
-    
-    if (strlen(dName) == strlen(startPath)) {
-      int flag = 0;
-      for (int i = 0; i < strlen(dName); i++) {
-        if (dName[i] != startPath[i]) {
-          flag = 1;
-          printf("%s\n", fName); // match is found, print the whole pathname of link
-          count++;
-          break;
-        }
+// KMP algorithm for string matching
+void preSufArr(char *pat, int q, int *processPattern)
+{
+  int len = 0;
+  processPattern[0] = 0;
+  int l = 1;
+  while (l < q)
+  {
+    if (pat[l] == pat[len])
+    {
+      len++;
+      processPattern[l] = len;
+      l++;
+    }
+    else
+    {
+      if (len != 0)
+        len = processPattern[len - 1];
+      else
+      {
+        processPattern[l] = 0;
+        l++;
       }
     }
+  }
+}
+
+void KMPAlgo(char *txt, char *pattrn,
+             const char *fname)
+{
+  int A = strlen(pattrn);
+  int B = strlen(txt);
+  int processPattern[A];
+  preSufArr(pattrn, A, processPattern);
+  int C = 0;
+  int D = 0;
+  while (C < B)
+  {
+    if (pattrn[D] == txt[C])
+    {
+      C++;
+      D++;
+    }
+    if (D == A)
+    {
+      count++;
+      D = processPattern[D - 1];
+      printf("%s\n", fname);
+    }
+    else if (C < B && pattrn[D] != txt[C])
+    {
+      if (D != 0)
+        D = processPattern[D - 1];
+      else
+        C = C + 1;
+    }
+  }
+}
+
+int customNftw(const char *fName,
+               const struct stat *statPtr, int fileFlags, struct FTW *pfwt)
+{
+  if (fileFlags == FTW_SL)
+  { // if it is a symbolic link
+    char buf[4096];
+    realpath(fName, buf);
+    char *file = strdup(fName);
+    char buf2[1024];
+    ssize_t len = readlink(file, buf2, 1023);
+    buf2[len] = '\0';
+    KMPAlgo(dirname(file), buf2, fName); // Pattern matching to check if parent or not
   }
   return 0;
 }
 
-int main(int argc, char * argV[]) {
+int main(int argc, char *argV[])
+{
   globalArgV = argV;
-  char * currentWorkingDir = getcwd(NULL, 0);
-  for (int i = 0; i < argc; i++) {
-    if (argc == 2) {
+  char *currentWorkingDir = getcwd(NULL, 0);
+  for (int i = 0; i < argc; i++)
+  {
+    if (argc == 2)
+    {
       target = i;
     }
   }
 
-  if (target <= -1) {
+  if (target <= -1)
+  {
     target = argc;
     startPath = currentWorkingDir;
-  } else {
+  }
+  else
+  {
     startPath = argV[target];
   }
   int fd_limit = 99;
-  int flags = FTW_PHYS; // FTW_PHYS to perform a physical walk 
+  int flags = FTW_PHYS; // FTW_PHYS to perform a physical walk
   int ret;
   ret = nftw(startPath, customNftw, fd_limit, flags);
   printf("%d\n", count);
